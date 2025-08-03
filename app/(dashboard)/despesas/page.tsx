68b336d7486645337354f3b41c7bfcb3ea7c5f51
Almaz Digital <almazdeveloper@gmail.com>
2025-08-03 15:36:42 -0300

    fix

--- app/(dashboard)/despesas/page.tsx
+++ app/(dashboard)/despesas/page.tsx
@@ -199,6 +199,10 @@ export default function DespesasPage() {
                       <SelectItem value="manutencao">Manutenção</SelectItem>
                       <SelectItem value="infraestrutura">Infraestrutura</SelectItem>
                       <SelectItem value="ministerios">Ministérios</SelectItem>
+                      <SelectItem value="agua ">Agua</SelectItem>
+                      <SelectItem value="luz ">Luz</SelectItem>
+                      <SelectItem value="energia">Energia</SelectItem>
+                      <SelectItem value="combustivel">Combustivel</SelectItem>
                       <SelectItem value="outros">Outros</SelectItem>
                     </SelectContent>
                   </Select>
@@ -384,10 +388,10 @@ export default function DespesasPage() {
                     <TableCell>
                       <span
                         className={`inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium ${despesa.status === "Pago"
-                            ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300"
-                            : despesa.status === "Pendente"
-                              ? "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300"
-                              : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300"
+                          ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300"
+                          : despesa.status === "Pendente"
+                            ? "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300"
+                            : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300"
                           }`}
                       >
                         {despesa.status}
